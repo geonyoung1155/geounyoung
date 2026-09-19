05d0a41cfec4d74673a6dce550dbb0cb95e9fc33
고건영 <youngstudy628@gmail.com>
2026-09-19 11:15:58 +0900

    환경 설정용 파일 추가. 실행과 디버그 테스트

---
+++ 1.변수.py
@@ -0,0 +1,20 @@
+a = 123
+print(a)
+
+a = "a"
+print(a)
+
+a = 119 + 119
+print(a)
+
+a = [123, 119, "kk", 6+2+8]
+print(a)
+
+var = 10
+var1 = 20
+
+a = var + var1
+print(a)
+
+input_var = input("숫자를 하나 입력하시요")
+print(input_var)
